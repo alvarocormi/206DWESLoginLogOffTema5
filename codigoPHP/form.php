3900ad916f61a659f9a70ebc016b45d3ed748791
alvarocormi <alvarocormin@gmail.com>
2023-12-05 12:36:26 +0100

    Extraido el formulario a un fichero aparte e importado en login.php

---
+++ codigoPHP/form.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- Iconos-->
+	<link rel="shortcut icon" href="../../webroot/img/palma.png" type="image/x-icon" />
+	<!--CSS-->
+	<link rel="stylesheet" href="../webroot/css/styleForm.css" />
+	<!-- TITULO -->
+	<title>Login- Alvaro Cordero Miñambres</title>
+</head>
+
+<body>
+	<div class="center">
+		<h1>Login</h1>
+		<form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+			<div class="txt_field">
+				<input type="text" id="usuario" name="usuario" value="<?php echo (isset($_REQUEST['usuario']) ? $_REQUEST['usuario'] : ''); ?>">
+				<span class="raya"></span>
+				<label>Usuario: </label>
+
+			</div>
+			<div class=" txt_field">
+				<input type="password" id="contrasena" name="contrasena" value="<?php echo (isset($_REQUEST['contrasena']) ? $_REQUEST['contrasena'] : ''); ?>">
+				<span class="raya"></span>
+				<label>Password: </label>
+
+			</div>
+
+			<p style="color: red;"><?php echo (!empty($aErrores["usuario"]) ? $aErrores["usuario"] : ''); ?>.</p>
+			<input type="submit" value="Iniciar Sesion" name="enviar">
+		</form>
+	</div>
+</body>
+
+</html>
--- codigoPHP/login.php
+++ codigoPHP/login.php
@@ -1,239 +1,177 @@
 <?php
-// Iniciar la sesión
+
+/**
+ * @author Alvaro Cordero <https://github.com/alvarocormi>
+ * @author Ismael Ferreras <https://github.com/IsmaelFG>
+ * @version 1.0
+ * @since 05-12-2023
+ * 
+ * @Annotation Proyecto LoginLogOFF Alvaro Cordero
+ */
+
+/**
+ * @link https://www.php.net/manual/function.session-start.php
+ * 
+ * Iniciamos la sesion mediante session_start()
+ * La funcion session_start() -> Iniciar una nueva sesión o reanudar la existente
+ */
 session_start();
-?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!-- Iconos-->
-    <link rel="shortcut icon" href="../webroot/img/palma.png" type="image/x-icon" />
-    <!--CSS-->
-    <link rel="stylesheet" href="../webroot/css/styleForm.css" />
+/**
+ * Inlucimos la libreria de validacion de formularios
+ * Incluimos la configuracion de la base de datos
+ */
+require_once('../core/231018libreriaValidacion.php');
+require_once('../config/confDBPDO.php');
 
-    <title>Login- Alvaro Cordero Miñambres</title>
-</head>
 
-<body>
-    <div class="center">
-        <h1>Login</h1>
-        <?php
-        /**
-         * @author Ismael, Alvaro
-         * 
-         * Inlucimos la libreria de validacion de formularios
-         * Incluimos la configuracion de la base de datos
-         */
-        require_once('../core/231018libreriaValidacion.php');
-        require_once('../config/confDBPDO.php');
+//Esta variable booleana la usaremos para indicar si las respuestas son correctas
+$entradaOK = true;
 
-        //Esta variable booleana la usaremos para indicar si las respuestas son correctas
-        $entradaOK = true;
+/**
+ * Almacenamos los errores del usuario en un array asociativo
+ * Estos errores van a controlar la auteticacion del usuario
+ * Guardaremos un mensaje en cada uno de ellos cuando ocurra un error en el mismo
+ */
+$aErrores = [
+    'usuario' => '',
+    'contrasena' => ''
+];
 
-        // Inicializamos la fecha actual ya que es un campo deshabilitado
-        $_REQUEST['fecha_deshabilitada'] = date('Y-m-d - H:i:s');
+/**
+ * @link https://www.php.net/manual/function.isset.php
+ * 
+ * Comprobamos mediante la funcion isset si el usuario le ha dado a enviar 
+ * La funcion isset() Determina si una variable está definida y no es null .
+ */
+if (isset($_REQUEST['enviar'])) {
 
-        // Almacena las respuestas
-        $aRespuestas = [
-            'usuario' => '',
-            'contrasena' => ''
-        ];
+    /**
+     * Validamos el usuario 
+     * Validamos la contrseña
+     * 
+     * Todas estas validaciones estan realizdas usando la libreria de validacion
+     * comprobarAlfanumerico() -> Funcion que compueba si el parametro recibido esta compuesto por caracteres alfabeticos y numericos conjuntamente.
+     * validarPassword() -> Funcion que compueba si el parametro recibido es una contraseña valida.
+     */
+    $aErrores['usuario'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['usuario'], 32, 4, 1);
+    $aErrores['contrasena'] = validacionFormularios::validarPassword($_REQUEST['contrasena'], 32, 4, 2, 1);
 
-        // Almacena los errores
-        $aErrores = [
-            'usuario' => '',
-            'contrasena' => ''
-        ];
+    /**
+     * @link https://www.php.net/manual/pdo.construct.php
+     * 
+     * Iniciamos la conexion con la base de datos mediante la clase PDO
+     * DNS -> Host de la base de datos
+     * USERNAME -> Usuario de la base de datos
+     * PASSWORD -> Contraseña de la abse de datos
+     */
+    $miDB = new PDO(DSN, USERNAME, PASSWORD);
 
-        /**
-         * @link https://www.php.net/manual/function.isset.php
-         * 
-         * Comprobamos mediante la funcion isset si el usuario le ha dado a enviar 
-         * La funcion isset() Determina si una variable está definida y no es null .
-         */
-        if (isset($_REQUEST['enviar'])) {
+    //Recogemos el nombre del usuario
+    $usuario = $_REQUEST['usuario'];
 
-            /**
-             * Validamos el usuario 
-             * Validamos la contrseña
-             * 
-             * Todas estas validaciones estan realizdas usando la libreria de validacion
-             * comprobarAlfanumerico() -> Funcion que compueba si el parametro recibido esta compuesto por caracteres alfabeticos y numericos conjuntamente.
-             * validarPassword() -> Funcion que compueba si el parametro recibido es una contraseña valida.
-             */
-            $aErrores['usuario'] = validacionFormularios::comprobarAlfaNumerico($_REQUEST['usuario'], 32, 4, 1);
-            $aErrores['contrasena'] = validacionFormularios::validarPassword($_REQUEST['contrasena'], 32, 4, 2, 1);
+    //Recogemos la contraseña del usuario
+    $contrasena = $_REQUEST['contrasena'];
 
-            /**
-             * @link https://www.php.net/manual/pdo.construct.php
-             * 
-             * Iniciamos la conexion con la base de datos mediante la clase PDO
-             * DNS -> Host de la base de datos
-             * USERNAME -> Usuario de la base de datos
-             * PASSWORD -> Contraseña de la abse de datos
-             */
-            $miDB = new PDO(DSN, USERNAME, PASSWORD);
+    /**
+     * @link https://www.php.net/manual/function.hash.php
+     * 
+     * Creamos una nueva contraseña mediante la funcion hash()
+     * Le pasamos como parametro el usuario y la contraseña
+     * 
+     * hash() -> Generar un valor hash (resumen de mensaje)
+     */
+    $hashContrasena = hash('sha256', $usuario . $contrasena);
 
-            //Recogemos el nombre del usuario
-            $usuario = $_REQUEST['usuario'];
+    /**
+     * Preparamos la consulta de seleccion
+     * Para comprobar que el usuario y la contraseña son correctos
+     */
+    $stmt = $miDB->prepare("SELECT * FROM T01_Usuario WHERE T01_CodUsuario = :usuario AND T01_Password = :hashContrasena");
 
-            //Recogemos la contraseña del usuario
-            $contrasena = $_REQUEST['contrasena'];
+    //Ejecutamos la consulta
+    $stmt->execute(['usuario' => $usuario, 'hashContrasena' => $hashContrasena]);
 
-            /**
-             * @link https://www.php.net/manual/function.hash.php
-             * 
-             * Creamos una nueva contraseña mediante la funcion hash()
-             * Le pasamos como parametro el usuario y la contraseña
-             * 
-             * hash() -> Generar un valor hash (resumen de mensaje)
-             */
-            $hashContrasena = hash('sha256', $usuario . $contrasena);
+    /**
+     * @link https://www.php.net/manual/pdostatement.fetchobject.php
+     * 
+     * Almacenamos el resultado de la query como objetos mediante la funcion fecthObject()
+     * fetchObject() -> Obtiene la siguiente fila y la devuelve como un objeto
+     */
+    $oUsuarioActivo = $stmt->fetchObject();
 
-            /**
-             * Preparamos la consulta de seleccion
-             * Para comprobar que el usuario y la contraseña son correctos
-             */
-            $stmt = $miDB->prepare("SELECT * FROM T01_Usuario WHERE T01_CodUsuario = :usuario AND T01_Password = :hashContrasena");
+    //Si no es resultado o no devuelve nada
+    if (!$oUsuarioActivo) {
 
-            //Ejecutamos la consulta
-            $stmt->execute(['usuario' => $usuario, 'hashContrasena' => $hashContrasena]);
+        //Ponemos la entradaOk a false
+        $entradaOK = false;
+    }
 
-            /**
-             * @link https://www.php.net/manual/pdostatement.fetchobject.php
-             * 
-             * Almacenamos el resultado de la query como objetos mediante la funcion fecthObject()
-             * fetchObject() -> Obtiene la siguiente fila y la devuelve como un objeto
-             */
-            $oUsuarioActivo = $stmt->fetchObject();
+    // Recorre aErrores para ver si hay algun error
+    foreach ($aErrores as $campo => $valor) {
 
-            //Si no es resultado o no devuelve nada
-            if (!$oUsuarioActivo) {
-
-                //Ponemos la entradaOk a false
-                $entradaOK = false;
-            }
-
-            // Recorre aErrores para ver si hay algun error
-            foreach ($aErrores as $campo => $valor) {
-
-                //Si el valor es distinto de null
-                if ($valor != null) {
-
-                    //Ponemos la entradaOK a false
-                    $entradaOK = false;
-
-                    // Limpiamos el campo
-                    $_REQUEST[$campo] = '';
-                }
-            }
-
-            //Si el usuario no ha pulsado el boton enviar 
-        } else {
+        //Si el valor es distinto de null
+        if ($valor != null) {
 
             //Ponemos la entradaOK a false
             $entradaOK = false;
+
+            // Limpiamos el campo
+            $_REQUEST[$campo] = '';
         }
+    }
 
-        //En caso de que '$entradaOK' sea true, cargamos las respuestas en el array '$aRespuestas' 
-        if ($entradaOK) {
+    //Si el usuario no ha pulsado el boton enviar 
+} else {
 
-            //Almacenamos las respuestas del usuario
-            $aRespuestas['usuario'] = $_REQUEST['usuario'];
-            $aRespuestas['contrasena'] = $_REQUEST['contrasena'];
+    //Ponemos la entradaOK a false
+    $entradaOK = false;
+}
 
-            //Incrementamos el número de conexiones
-            $numConexiones = $oUsuarioActivo->T01_NumConexiones + 1;
+//En caso de que '$entradaOK' sea true, cargamos las respuestas en el array '$aRespuestas' 
+if ($entradaOK) {
 
-            //Actualizamos la fecha y hora de la última conexión
-            $fechaHoraUltimaConexion = $oUsuarioActivo->T01_FechaHoraUltimaConexion;
+    //Incrementamos el número de conexiones
+    $numConexiones = $oUsuarioActivo->T01_NumConexiones + 1;
 
-            /**
-             * Realizamos la consulta de aztualizacion
-             * Mediante esta consulta vamos a actualizar el numero de conexiones de la base de datos del usuario
-             */
-            $miDB->query("UPDATE T01_Usuario SET T01_NumConexiones = $numConexiones, T01_FechaHoraUltimaConexion = CURRENT_TIMESTAMP WHERE T01_CodUsuario = '$usuario'");
+    /**
+     * Actualizamos la fecha y la hora de la ultima conexion
+     * Lo guardamos en una variable y no lo hacemos directamente en la consulta de actualizacion
+     * Con el objetivo de guardar la fecha y hora de ulima conexion anterioir
+     */
+    $fechaHoraUltimaConexion = $oUsuarioActivo->T01_FechaHoraUltimaConexion;
 
-            /**
-             * Configuramos las sesiones para almacenar los datos del usuario
-             * Lo realizamos mediante la variable $_SESSION
-             */
-            $_SESSION['usuario'] = $oUsuarioActivo->T01_CodUsuario;
-            $_SESSION['numConexiones'] = $numConexiones;
-            $_SESSION['ultimaConexion'] = $fechaHoraUltimaConexion;
+    /**
+     * Realizamos la consulta de aztualizacion
+     * Mediante esta consulta vamos a actualizar el numero de conexiones de la base de datos del usuario
+     * Lo realizamos mediante Current_TimeStamp para que nos coga la fecha y la hora actual
+     */
+    $miDB->query("UPDATE T01_Usuario SET T01_NumConexiones = $numConexiones, T01_FechaHoraUltimaConexion = CURRENT_TIMESTAMP WHERE T01_CodUsuario = '$usuario'");
 
-            /**
-             * Redirigimos al usuario a la pagina Progam.php
-             * Para echo usamos la etiqueta <meta/> y le pasamos como parametro la url del fichero
-             */
-            echo '<meta http-equiv="refresh" content="0;url=Programa.php">';
+    /**
+     * Configuramos las sesiones para almacenar los datos del usuario
+     * Lo realizamos mediante la variable $_SESSION
+     */
+    $_SESSION['usuario'] = $oUsuarioActivo->T01_CodUsuario;
+    $_SESSION['numConexiones'] = $numConexiones;
+    $_SESSION['ultimaConexion'] = $fechaHoraUltimaConexion;
 
-            /**
-             * @link https://www.php.net/manual/function.exit.php
-             * 
-             * Cerramos la ejecucion del progama
-             * Mediante exit podremos cerrar la ejecucion del progama
-             */
-            exit();
+    /**
+     * Redirigimos al usuario a la pagina Progam.php
+     * Para echo usamos la etiqueta <meta/> y le pasamos como parametro la url del fichero
+     */
+    echo '<meta http-equiv="refresh" content="0;url=Programa.php">';
 
-            //Si el fromulario a sido enviado pero el usuario o contrasena no ha sido valdiado 
-        } else {
+    /**
+     * @link https://www.php.net/manual/function.exit.php
+     * 
+     * Cerramos la ejecucion del progama
+     * Mediante exit podremos cerrar la ejecucion del progama
+     */
+    exit();
 
-            /**
-             * Mediante la funcion isset() comprobamos si el usuario le ha dado a enviar y si el resultado es true
-             * la funcion isset() -> Determina si una variable está definida y no es null .
-             */
-            if (isset($_REQUEST['enviar']) && !$oUsuarioActivo) {
-                // Mostramos un mensaje de error y el formulario nuevamente
-        ?>
-
-                <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <div class="txt_field">
-                        <input type="text" id="usuario" name="usuario" value="<?php echo (isset($_REQUEST['usuario']) ? $_REQUEST['usuario'] : ''); ?>">
-                        <span class="raya"></span>
-                        <label>Usuario: </label>
-
-                    </div>
-                    <div class=" txt_field">
-                        <input type="password" id="contrasena" name="contrasena" value="<?php echo (isset($_REQUEST['contrasena']) ? $_REQUEST['contrasena'] : ''); ?>">
-                        <span class="raya"></span>
-                        <label>Password: </label>
-
-                    </div>
-
-                    <p class='error' style="color: red; font-size: 15px; margin-bottom: 15px; ">Usuario o contrasena incorrectos. Inténtalo de nuevo.</p>
-                    <input type="submit" value="Iniciar Sesion" name="enviar">
-                </form>
-            <?php
-            } else {
-                // Formulario que se le muestra al cliente para que lo rellene
-            ?>
-                <form name="formulario" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                    <div class="txt_field">
-                        <input type="text" id="usuario" name="usuario" value="<?php echo (isset($_REQUEST['usuario']) ? $_REQUEST['usuario'] : ''); ?>">
-                        <span class="raya"></span>
-                        <label>Usuario: </label>
-
-                    </div>
-                    <div class=" txt_field">
-                        <input type="password" id="contrasena" name="contrasena" value="<?php echo (isset($_REQUEST['contrasena']) ? $_REQUEST['contrasena'] : ''); ?>">
-                        <span class="raya"></span>
-                        <label>Password: </label>
-
-
-                    </div>
-
-                    <input type="submit" value="Iniciar Sesion" name="enviar">
-                </form>
-    </div>
-<?php
-            }
-        }
-
-?>
-
-</body>
-
-</html>
+    //Si el fromulario a sido enviado pero el usuario o contraseña no ha sido valdiado 
+} else {
+    // Mostramos un mensaje de error y el formulario nuevamente
+    require_once("../codigoPHP/form.php");
+}
